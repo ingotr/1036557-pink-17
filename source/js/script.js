b16549d3443da83a80e1c0976cf397840fb6c3de
Aleksandr <markovkin88@gmail.com>
2019-08-10 21:56:07 +0300

    Добавляет интерактивное меню на главную страницу мобильной версии

---
+++ source/js/script.js
@@ -0,0 +1,16 @@
+var navMain = document.querySelector(".main-nav");
+var navBurger = document.querySelector(".main-nav__burger");
+
+navMain.classList.remove("main-nav--nojs");
+navMain.classList.remove("main-nav--opened");
+navMain.classList.add("main-nav--closed");
+
+navBurger.addEventListener("click", function() {
+  if (navMain.classList.contains("main-nav--closed")) {
+    navMain.classList.remove("main-nav--closed");
+    navMain.classList.add("main-nav--opened");
+  } else {
+    navMain.classList.add("main-nav--closed");
+    navMain.classList.remove("main-nav--opened");
+  }
+})
