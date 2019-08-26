651067554559a5670d5fdd40eacdbd04e86cbf49
Aleksandr <markovkin88@gmail.com>
2019-08-26 23:28:23 +0300

    Добавляет зависимости для минификации стилей. Добавляет таски для копирование ресурсов.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 "use strict";
 
 var gulp = require("gulp");
+var del = require("del");
 var plumber = require("gulp-plumber");
 var sourcemap = require("gulp-sourcemaps");
 var sass = require("gulp-sass");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var csso = require("gulp-csso");
+var rename = require("gulp-rename");
 var server = require("browser-sync").create();
 var webp = require("gulp-webp");
 
@@ -14,13 +17,13 @@ gulp.task("css", function () {
     .pipe(plumber())
     .pipe(sourcemap.init())
     .pipe(sass())
-    .pipe(postcss([
-      autoprefixer()
-    ]))
+    .pipe(postcss([ autoprefixer() ]))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
     .pipe(gulp.dest("source/css"))
     .pipe(server.stream());
-});
+})
 
 gulp.task("server", function () {
   server.init({
@@ -42,3 +45,19 @@ gulp.task("webp", function () {
     .pipe(webp({quality: 90}))
     .pipe(gulp.dest("source/img"));
 });
+
+gulp.task("copy", function () {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/js/**",
+    "source/*.ico"
+  ], {
+    base: "source"
+  })
+  .pipe(gulp.dest("build"));
+})
+
+gulp.task("clean", function () {
+  return del("build");
+})
